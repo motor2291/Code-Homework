94063165d840f82142106fdf248853322634d48d
Notch Nien 粘政緯 <notch.nien@bonuswinner.com.tw>
2024-03-22 15:47:09 +0800

    HW1 0322

---
+++ HW1 - 0322/JSON/Program.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        string json = File.ReadAllText("D:/Project/JSON/IterateParseJSON.json");
+
+        var stack = new Stack<KeyValuePair<string, JToken>>();
+        stack.Push(new KeyValuePair<string, JToken>("", JObject.Parse(json)));
+
+        while (stack.Count > 0)
+        {
+            var (parentKey, token) = stack.Pop();
+
+            //用來印出widget、window、images、text
+            if (token.Type != JTokenType.String && token.Type != JTokenType.Integer)
+            {
+                if (token.Parent != null)
+                {
+                    if (token.Parent.Type != JTokenType.Array)
+                    {
+                        Console.WriteLine($"{parentKey}");
+                    }
+                }
+            }
+
+            //印出其餘的Key-Value值
+            if (token.Type == JTokenType.Object)
+            {
+                var obj = (JObject)token;
+                foreach (var prop in obj.Properties().Reverse())
+                {
+                    stack.Push(new KeyValuePair<string, JToken>(prop.Name, prop.Value));
+                }
+            }
+            else if (token.Type == JTokenType.Array)
+            {
+                var arr = (JArray)token;
+                for (int i = arr.Count - 1; i >= 0; i--)
+                {
+                    stack.Push(new KeyValuePair<string, JToken>($"[{i}]", arr[i]));
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{parentKey}: {token}");
+            }
+        }
+    }
+}
---
+++ HW1 - 0322/RecursiveJSON/Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string json = @"
+            [
+             {
+             ""Name"": ""Jose Luis"",
+             ""Channel"": ""ParametricCamp"",
+             ""Active"": true
+             }
+            ]";
+
+        var jsonArray = JArray.Parse(json);
+
+        ParseJson(jsonArray);
+    }
+
+    static void ParseJson(JToken token)
+    {
+        if (token.Type == JTokenType.Object)
+        {
+            ParseJsonObject((JObject)token);
+        }
+        else if (token.Type == JTokenType.Array)
+        {
+            ParseJsonArray((JArray)token);
+        }
+        else
+        {
+            Console.WriteLine(token.ToString());
+        }
+    }
+
+    static void ParseJsonObject(JObject jsonObject)
+    {
+        foreach (var property in jsonObject.Properties())
+        {
+            Console.WriteLine($"Property: {property.Name}, Value: {property.Value}");
+            ParseJson(property.Value);
+        }
+    }
+
+    static void ParseJsonArray(JArray jsonArray)
+    {
+        foreach (var item in jsonArray)
+        {
+            ParseJson(item);
+        }
+    }
+}
